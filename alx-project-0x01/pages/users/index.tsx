bc3c938e853ddf0d2dcce839c205f961397888a7
olivier01A <oliviermasengesho590@gmail.com>
2025-09-18 19:05:13 +0200

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,11 @@
 
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
