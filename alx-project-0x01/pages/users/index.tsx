d3faa9d4c79848bbd0ebabee884368b24e126cc6
olivier01A <oliviermasengesho590@gmail.com>
2025-09-18 19:06:27 +0200

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,4 +1,4 @@
-
+["posts.map", "<UserCard"]
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users")
   const posts = await response.json()
